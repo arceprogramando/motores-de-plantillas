819bcbd53eb43263e2e795f518e0b7425660600d
Felipe Arce <arceprogramando@gmail.com>
2023-05-30 01:01:02 -0300

    first commit practice handlebar

--- src/app.js
+++ src/app.js
@@ -13,14 +13,67 @@ app.set('views', `${__dirname}/views`); // Ruta hacia la carpeta views
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
+    const PORT = 8080;
+
     const user = {
         name: "Felipe"
     };
 
     res.render('home', {
-        name: user.name
+        name: user.name,
+        PORT: PORT
     });
 });
+
+app.get('/food', (req, res) => {
+    const food = [
+        {
+            "name": "Cheeseburgers",
+            "price": 100
+        },
+        {
+            "name": "Veggie Burgers",
+            "price": 120
+        },
+        {
+            "name": "Chicken Burgers",
+            "price": 90
+        },
+        {
+            "name": "Turkey Burgers",
+            "price": 110
+        },
+        {
+            "name": "Fish Burgers",
+            "price": 130
+        },
+        {
+            "name": "Mushroom Burgers",
+            "price": 95
+        },
+        {
+            "name": "Black Bean Burgers",
+            "price": 85
+        },
+        {
+            "name": "Lamb Burgers",
+            "price": 150
+        },
+        {
+            "name": "Salmon Burgers",
+            "price": 140
+        },
+        {
+            "name": "Bison Burgers",
+            "price": 160
+        }
+    ]
+
+    res.render('food', {
+        food,
+        PORT: PORT
+    })
+})
 
 
 const PORT = 8080;
